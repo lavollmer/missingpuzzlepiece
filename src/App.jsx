f1dfa8acd8e5b2a7e9a89c58eaa3c217d5920529
Laura <laura.e.vollmer@gmail.com>
2024-05-06 10:56:44 -0500

    adding question and homepage with router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
+import { Route, Switch } from "react-router-dom";
 import HomePage from "./assets/HomePage.jsx";
+import Question from "./assets/Question.jsx";
 
 function App() {
-
   return (
     <>
-      <HomePage />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+        </Route>
+        <Route exact path="/question">
+          <Question />
+        </Route>
+      </Switch>
     </>
   );
 }
