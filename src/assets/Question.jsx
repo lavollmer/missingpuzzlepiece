0ada9d5ba5be5097d197f5de2905ed5d374002dc
Laura <laura.e.vollmer@gmail.com>
2024-05-06 16:27:31 -0500

    added puzzle questions to question component

--- src/assets/Question.jsx
+++ src/assets/Question.jsx
@@ -15,6 +15,24 @@ const Question = () => {
   const questions = [
     "Have you looked around your area?",
     "Have you checked under the table?",
+    "Have you checked the puzzle packaging?", 
+    "Have you checked the plastic wrap that the puzzle comes in?",
+    "Have you checked the puzzle box?",
+    "Have you checked the puzzle bag?",
+    "Have you checked under the chairs?",
+    "Have you checked under the couch?",
+    "Have you checked your pockets?",
+    "Have you checked the counter?",
+    "Have you checked every room that the puzzle was physically in?",
+    "Have you vacuumed the area? Have you checked the vacuum bag?", 
+    "Have you checked the trash can? Have you checked the trash bag?",  
+    "Have you checked the laundry basket?",
+    "If you have children, have you checked their toy box?",
+    "If you have a pet, have you checked their bed?",
+    "If you have a pet, have you checked their paw?",
+    "If others are aware you are working on the puzzle, is someone pulling a prank on you?",
+    "If all the other pieces are placed, are they where they belong on the puzzle?",
+    "Pause, take a moment to breath."
   ];
 
   const handleNoClick = () => {
