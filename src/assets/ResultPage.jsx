56ee0ea653157edc6b96dab4136baf698bd5318e
Laura <laura.e.vollmer@gmail.com>
2024-05-06 16:14:02 -0500

    adding ResultPage

--- src/assets/ResultPage.jsx
+++ src/assets/ResultPage.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const ResultPage = () => {
+  const navigate= useNavigate();
+
+  const handleClick = () => {
+    navigate("/");
+  }
+
   return (
     <div>
       <h1>Congrats! Continue puzzling.</h1>
       <h1>Thank you for using the Missing Puzzle Piece application!</h1>
       <div>This many puzzle pieces saved!</div>
+      <button onClick={handleClick}>Return Home</button>
     </div>
   );
 };
