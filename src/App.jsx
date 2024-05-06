1655e74397ecddf2adcfbab4e6f6bcf984a026e0
Laura <laura.e.vollmer@gmail.com>
2024-05-06 12:07:55 -0500

    debugging found result page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import HomePage from "./assets/HomePage.jsx";
 import Question from "./assets/Question.jsx";
+import Result from "./assets/ResultPage.jsx";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route exact path="/question" element={<Question />} />
+        <Route exact path="/result" element={<Result />} />
       </Routes>
     </>
   );
--- src/assets/Question.jsx
+++ src/assets/Question.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import FoundButton from "./FoundButton.jsx";
 import NoButton from "./NoButton.jsx";
+import { useNavigate } from "react-router-dom";
 
 const Question = () => {
+  const navigate = useNavigate();
+
   const handleFoundClick = () => {
     alert("Button found!");
+    navigate("/result");
   };
 
   const handleNoClick = () => {
