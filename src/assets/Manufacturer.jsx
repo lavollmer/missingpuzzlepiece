2e111a20be20d87872a1f1ee0b6876b35219a92e
Laura <laura.e.vollmer@gmail.com>
2024-05-07 11:57:22 -0500

    adding more manufacturer links

--- src/assets/Manufacturer.jsx
+++ src/assets/Manufacturer.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React from "react";
 
 const Manufacturer = () => {
   return (
     <div>
-        <h1>Contact the Manufacturer</h1>
-        <p>Below are a list of the most popular puzzle manufacturers in the USA</p>
+      <h1>Contact the Manufacturer</h1>
+      <p>
+        Below are a list of the most popular puzzle manufacturers in the USA.
+      </p>
+      <h2>Ravensburger</h2>
+      <a
+        href="https://www.ravensburger.us/service/contact-us/index.html"
+        rel="nooperner noreferrer"
+      >
+        Ravensburger Contact Us
+      </a>
+      <h2>Cobble Hill</h2>
+      <a
+        href="https://www.cobblehillpuzzles.com/pages/puzzle-issues"
+        rel="nooperner noreferrer"
+      >
+        Cobble Hill Contact Us
+      </a>
+      <h2>Buffalo Games</h2>
+      <a
+        href="https://buffalogames.com/missing-puzzle-pieces/?gad_source=1&gclid=CjwKCAjwouexBhAuEiwAtW_Zx1ANpnIA9iVzR9R3F4K4U_002syDsYKjwuBWMKClXeSua4l5lK6ZLhoCg0sQAvD_BwE"
+        rel="nooperner noreferrer"
+      >
+        Buffalo Games Contact Us
+      </a>
+      <h2>Pomegranate</h2>
+      <a
+        href="https://www.pomegranate.com/pages/contact-us?gad_source=1&gclid=CjwKCAjwouexBhAuEiwAtW_Zxx-93rvro5Ra0rX1EmjqXQcJcoTbFbiKMfypHEG8f9Om9h1rKRfLURoCy1cQAvD_BwE"
+        rel="nooperner noreferrer"
+      >
+        Pomegranate Contact Us
+      </a>
     </div>
-  )
-}
+  );
+};
 
-export default Manufacturer
+export default Manufacturer;
