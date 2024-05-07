e42552f0b80344d78caddf7bcf745f44b5fcc4bf
Laura <laura.e.vollmer@gmail.com>
2024-05-07 12:15:24 -0500

    making a puzzle piece content complete

--- src/assets/MakingPiece.jsx
+++ src/assets/MakingPiece.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const MakingPiece = () => {
   return (
-    <div>MakingPiece</div>
-  )
-}
+    <div>
+      <h1>Making a Puzzle Piece</h1>
+      <p>Below are instructions on how to make a puzzle piece.</p>
+      <h2>Materials</h2>
+      <ul>
+        <li>Cardboard</li>
+        <li>Scissors or Box cutter</li>
+        <li>Glue</li>
+        <li>Pen</li>
+        <li>Marker</li>
+      </ul>
+      <h2>Instructions</h2>
+      <ol>
+        <li>
+          Find a piece of cardboard that is similar in thickness to the puzzle
+          piece you are missing.
+        </li>
+        <li>
+          Place the puzzle piece on top of the cardboard and trace around the
+          piece with a pen or marker.
+        </li>
+        <li>
+          Cut out the shape of the puzzle piece with scissors or a box cutter
+          safely.
+        </li>
+        <li>Color or add the image of the piece with glue to the cardboard.</li>
+      </ol>
+    </div>
+  );
+};
 
-export default MakingPiece
+export default MakingPiece;
