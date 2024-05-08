ef5243ee2138cc25ff98517db5c47379c76b945e
Laura <laura.e.vollmer@gmail.com>
2024-05-08 12:30:12 -0500

    updating questions

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div className='text-white text-center text-sm md:text-md'>Copyright 2024. A creation by Laura Developer.</div>
+    <div className='text-white text-center text-sm md:text-md'>Copyright 2024. A creation by Laura Developer. An application made for puzzlers developer by a puzzler.</div>
   )
 }
 
