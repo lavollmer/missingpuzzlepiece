cb3c5cedaaf85524e2c8b37601dacf3fd147935d
Laura <laura.e.vollmer@gmail.com>
2024-05-09 14:04:15 -0500

    styling the manufacturer page

--- src/assets/HomePage.jsx
+++ src/assets/HomePage.jsx
@@ -11,7 +11,7 @@ const HomePage = () => {
   return (
     <>
       <div className="flex flex-col items-center text-white text-center text-xl md:text-2xl space-y-6 mt-20 px-6 py-6">
-        <h1 className="text-2xl md:text-4xl text-bold mb-2">The Missing Puzzle Piece</h1>
+        <h1 className="text-2xl md:text-4xl text-bold mb-2">Find My Puzzle Piece</h1>
         <p className="text-md md:text-lg text-green-500 mb-6">A Free Application</p>
         <p className="text-lg md:text-xl mb-8">Lost a puzzle piece? Let us help!</p>
         <button onClick={handleClick} className='bg-blue-500 hover:bg-blue-700 text-white text-md font-bold py-2 px-4 rounded'>Start</button>
--- src/assets/Manufacturer.jsx
+++ src/assets/Manufacturer.jsx
@@ -20,6 +20,7 @@ const handleClick = () => {
       <a
         href="https://www.ravensburger.us/service/contact-us/index.html"
         rel="nooperner noreferrer"
+        className="underline"
       >
         Ravensburger Contact Us
       </a>
@@ -27,6 +28,7 @@ const handleClick = () => {
       <a
         href="https://www.cobblehillpuzzles.com/pages/puzzle-issues"
         rel="nooperner noreferrer"
+        className="underline"
       >
         Cobble Hill Contact Us
       </a>
@@ -34,6 +36,7 @@ const handleClick = () => {
       <a
         href="https://buffalogames.com/missing-puzzle-pieces/?gad_source=1&gclid=CjwKCAjwouexBhAuEiwAtW_Zx1ANpnIA9iVzR9R3F4K4U_002syDsYKjwuBWMKClXeSua4l5lK6ZLhoCg0sQAvD_BwE"
         rel="nooperner noreferrer"
+        className="underline"
       >
         Buffalo Games Contact Us
       </a>
@@ -41,12 +44,12 @@ const handleClick = () => {
       <a
         href="https://www.pomegranate.com/pages/contact-us?gad_source=1&gclid=CjwKCAjwouexBhAuEiwAtW_Zxx-93rvro5Ra0rX1EmjqXQcJcoTbFbiKMfypHEG8f9Om9h1rKRfLURoCy1cQAvD_BwE"
         rel="nooperner noreferrer"
+        className="underline"
       >
         Pomegranate Contact Us
       </a>
-      <div>
-        <h2>Need More Help</h2>
-        <p className="font-bold text-green-500">I would like to make my own puzzle piece.</p>
+      <div className="flex flex-col items-center text-white text-center text-xl md:text-2xl space-y-6 mt-20 px-6 py-6">
+        <h2 className="font-bold text-green-500 ">I would like to make my own puzzle piece.</h2>
         <button onClick={handleClick} className='bg-blue-500 hover:bg-blue-700 text-white text-md py-2 px-4 rounded'>Making a Puzzle Piece</button>
       </div>
     </div>
