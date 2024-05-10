5a416dbc959ed9d767a1933151870ecb9cbb7a49
Laura <laura.e.vollmer@gmail.com>
2024-05-10 12:33:46 -0500

    creating feedback form

--- src/assets/Feedback.jsx
+++ src/assets/Feedback.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const Feedback = () => {
+  const [feedback, setFeedback] = useState('');
+
+  const handleSubmit = (e) => {
+    event.preventDefault();
+    console.log(feedback);
+  }
+
   return (
-    <div>Feedback</div>
+    <div>
+      <h1>Feedback Form</h1>
+      <p>Please feel out this feedback form so we can improve our site!</p>
+    </div>
   )
 }
 
