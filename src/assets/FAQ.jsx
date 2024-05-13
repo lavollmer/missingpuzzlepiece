569bf4717513c424e0e300dd98c1b1d7ecbfd265
Laura <laura.e.vollmer@gmail.com>
2024-05-13 15:20:31 -0500

    styling FAQ form

--- src/assets/FAQ.jsx
+++ src/assets/FAQ.jsx
@@ -11,18 +11,15 @@ const FAQ = () => {
   return (
     <div className="flex flex-col items-center text-white text-lg m-10">
       <h1 className="text-green-300">Frequently Asked Questions (FAQ)</h1>
-      <div className="flex flex-col items-left ">
-        <h2>What is Find My Puzzle Piece?</h2>
+      <div className="flex flex-col items-left">
+        <h2 className="text-blue-200">What is Find My Puzzle Piece?</h2>
         <p>
           Find My Puzzle Piece is a web application that helps users find a
-          missing puzzle piece. Users can upload an image of their puzzle and
-          the application will search for a matching puzzle piece in its
-          database.
+          missing puzzle piece. 
         </p>
-        <h2>How does Find My Puzzle Piece work?</h2>
-        <p></p>
-        <h2>How accurate is Find My Puzzle Piece?</h2>
-        <p></p>
+        <h2 className="text-blue-200">How does Find My Puzzle Piece work?</h2>
+        <p>Find My Puzzle Piece is a web application that helps users find a
+          missing puzzle piece. The user will go through a series of most common places where a puzzle piece is lost. If unable to find with the questions listed, then the user will be directed to a manufacturer contact site.</p>
       </div>
       <button
         onClick={handleClick}
