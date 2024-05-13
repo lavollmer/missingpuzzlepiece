d9d9938cd751f1ffe928e7a7a30349e8636d2931
Laura <laura.e.vollmer@gmail.com>
2024-05-13 14:58:49 -0500

    debugged feedback in footer

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -14,14 +14,8 @@ const Footer = () => {
       <div className="text-white text-center text-sm md:text-md">
         Copyright 2024. A creation by Laura Developer. An application made for
         puzzlers developer by a puzzler.
-      </div>
-      <div>
-        <a
-          onClick={handleClick}
-          className="bg-blue-500 hover:bg-blue-700 text-white text-md py-2 px-4 rounded"
-        >
-          Feedback
-        </a>
+        <a onClick={handleClick} className="text-green hover:text-yellow-100"> Have Feedback for Me?</a>
+        
       </div>
     </div>
   );
