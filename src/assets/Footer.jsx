fb474d4d4dc883824739dd4b190f8f2741ad47c4
Laura <laura.e.vollmer@gmail.com>
2024-05-13 14:50:19 -0500

    adding feedback link to footer

--- src/assets/Footer.jsx
+++ src/assets/Footer.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import React from "react";
+//importing navigate from react-router-dom
+import { useNavigate } from "react-router-dom";
+
 
 const Footer = () => {
-  return (
-    <div className='text-white text-center text-sm md:text-md'>Copyright 2024. A creation by Laura Developer. An application made for puzzlers developer by a puzzler.</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default Footer
+  const handleClick = () => {
+    navigate("/feedback");
+  };
+
+  return (
+    <div>
+      <div className="text-white text-center text-sm md:text-md">
+        Copyright 2024. A creation by Laura Developer. An application made for
+        puzzlers developer by a puzzler.
+      </div>
+      <div>
+        <Button
+          onClick={handleClick}
+          className="bg-blue-500 hover:bg-blue-700 text-white text-md py-2 px-4 rounded"
+        >
+          {" "}
+          Feedback{" "}
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
