477a2b3bdd784a733bad54f6316f948201253ced
Laura <laura.e.vollmer@gmail.com>
2024-05-16 00:21:58 -0500

    updating info section

--- src/assets/Info.jsx
+++ src/assets/Info.jsx
@@ -5,18 +5,20 @@ const Info = () => {
   // useNavigate is a hook provided by react-router-dom and ways of navigating between pages
   const navigate = useNavigate();
 
-  const handleClick = () => {
+  const handleQuestionClick = () => {
     navigate("/question");
   };
 
   return (
-    <div>
-      <h1>
+    <div className="flex flex-col text-white items-center justify-content m-10">
+      <h1 className="text-xl font-bold mb-10">Before You Begin</h1>
+      <h2 className="text-xl mb-10">
         You will be shown a series of questions (one at a time) to help find
-        your puzzle piece.Select the FOUND button if you find your puzzle piece.
-        Select the NO Button if you do not find your puzzle piece in the
-        location described.
-      </h1>
+        your puzzle piece.
+      </h2>
+      <h2 className="text-xl mb-10">Select the FOUND button if you have found your puzzle piece.</h2>
+      <h2 className="text-xl mb-10">Select the NO button if you have not found your puzzle piece in the location described.</h2>
+      <button onClick={handleQuestionClick} className='bg-blue-500 hover:bg-blue-700 text-white text-xl font-bold py-2 px-8 rounded'>START</button>
     </div>
   );
 };
