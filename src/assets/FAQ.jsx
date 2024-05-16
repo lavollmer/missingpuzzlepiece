5d6ee766cf5a17c31eada54f8f6087dcc4b73e6d
Laura <laura.e.vollmer@gmail.com>
2024-05-16 00:30:58 -0500

    formatting FAQ

--- src/assets/FAQ.jsx
+++ src/assets/FAQ.jsx
@@ -10,28 +10,43 @@ const FAQ = () => {
 
   return (
     <div className="flex flex-col items-center text-white text-lg m-10">
-      <h1 className="text-green-300">Frequently Asked Questions (FAQ)</h1>
-      <div className="flex flex-col items-left">
-        <h2 className="text-blue-200">What is the Find My Puzzle Piece Application?</h2>
-        <p>
+      <h1 className="text-green-300 mb-6 font-bold">
+        Frequently Asked Questions (FAQ)
+      </h1>
+      <div className="flex flex-col items-center mb-10">
+        <h2 className="text-blue-200 mb-2">
+          What is the Find My Puzzle Piece Application?
+        </h2>
+        <p className="mb-4">
           Find My Puzzle Piece is a web application that helps users find a
-          missing puzzle piece. Many puzzle pieces go missing and there are not many resources to help find them. 
+          missing puzzle piece. Many puzzle pieces go missing and there are not
+          many resources to help find them.
         </p>
-        <h2 className="text-blue-200">How does Find My Puzzle Piece Application work?</h2>
-        <p>
+        <h2 className="text-blue-200 mb-2">
+          How does Find My Puzzle Piece Application work?
+        </h2>
+        <p className="mb-4">
           Find My Puzzle Piece is a web application that helps users find a
           missing puzzle piece. The user will go through a series of most common
           places where a puzzle piece is lost. If unable to find with the
           questions listed, then the user will be directed to a manufacturer
-          contact site. If unable to contact the manufacturer, the user will be directed to making a puzzle piece.
+          contact site. If unable to contact the manufacturer, the user will be
+          directed to making a puzzle piece.
         </p>
-        <h2 className="text-blue-200">Why was Find My Puzzle Piece created?</h2>
-        <p>
-          Find My Puzzle Piece was created because the developer saw a need for it in the puzzle community. 
+        <h2 className="text-blue-200 mb-2">
+          Why was Find My Puzzle Piece created?
+        </h2>
+        <p className="mb-4">
+          Find My Puzzle Piece was created because the developer saw a need for
+          it in the puzzle community.
         </p>
-        <h2 className="text-blue-200">Does Fine My Puzzle Piece cost any money?</h2>
-        <p>
-          Find My Puzzle Piece is a FREE web application resource. If you enjoyed your experience, please submit feedback to help improve the product.
+        <h2 className="text-blue-200 mb-2">
+          Does Fine My Puzzle Piece cost any money?
+        </h2>
+        <p className="mb-4">
+          Find My Puzzle Piece is a FREE web application resource. If you
+          enjoyed your experience, please submit feedback to help improve the
+          product.
         </p>
       </div>
 
