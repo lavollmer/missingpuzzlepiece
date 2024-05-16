034cb124e0afff4ebf9f16087eeb74826290a1fd
Laura <laura.e.vollmer@gmail.com>
2024-05-16 01:33:50 -0500

    updating heroku functionality and package json

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css';
 import App from "./App.jsx";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router} from "react-router-dom";
-require('dotenv').config();
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
