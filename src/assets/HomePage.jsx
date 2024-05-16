327578d649b6c6d6e0405cb983ac1a25f055ec6e
Laura <laura.e.vollmer@gmail.com>
2024-05-16 00:11:26 -0500

    styling home page for accessibility

--- src/assets/HomePage.jsx
+++ src/assets/HomePage.jsx
@@ -17,10 +17,10 @@ const HomePage = () => {
     <>
       <div className="flex flex-col items-center text-white text-center text-xl md:text-2xl space-y-6 mt-20 px-6 py-6">
         <h1 className="text-4xl md:text-4xl text-bold mb-2">Find My <br/> Puzzle Piece</h1>
-        <p className="font-bold text-lg md:text-xl text-green-500 mb-6">A Free Application</p>
-        <p className="text-lg md:text-xl mb-8">Lost a puzzle piece? Let us help!</p>
-        <button onClick={handleClick} className='bg-blue-500 hover:bg-blue-700 text-white text-md font-bold py-2 px-4 rounded'>Start</button>
-        <button onClick={handleFAQClick} className='bg-purple-500 hover:bg-purple-700 text-white text-md font-bold py-2 px-4 rounded'>FAQ</button>
+        <p className="font-bold text-xl md:text-2xl text-green-500 mb-6">A Free Application</p>
+        <p className="text-xl md:text-2xl mb-8">Lost a puzzle piece? Let us help!</p>
+        <button onClick={handleClick} className='bg-blue-500 hover:bg-blue-700 text-white text-xl font-bold py-4 px-10 rounded'>Start</button>
+        <button onClick={handleFAQClick} className='bg-purple-500 hover:bg-purple-700 text-white text-xl font-bold py-4 px-10 rounded'>FAQ</button>
       </div>
     </>
   );
