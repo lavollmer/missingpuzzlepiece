8c062ce14dbdb792be636109c9af189d6344d056
Laura <laura.e.vollmer@gmail.com>
2024-05-23 19:54:24 -0500

    styling manufacturer page

--- src/assets/Manufacturer.jsx
+++ src/assets/Manufacturer.jsx
@@ -24,46 +24,54 @@ const Manufacturer = () => {
         your manufacturer is not listed, please Google your manufacturer or
         proceed to making a puzzle piece.
       </p>
-      <h2 className="text-xl md:text-2xl text-bold mb-2 text-yellow-300">
-        Ravensburger
-      </h2>
-      <a
-        href="https://www.ravensburger.us/service/contact-us/index.html"
-        rel="nooperner noreferrer"
-        className="underline"
-      >
-        Ravensburger Contact Us
-      </a>
-      <h2 className="text-xl md:text-2xl text-bold mb-2 text-yellow-300">
-        Cobble Hill
-      </h2>
-      <a
-        href="https://www.cobblehillpuzzles.com/pages/puzzle-issues"
-        rel="nooperner noreferrer"
-        className="underline"
-      >
-        Cobble Hill Contact Us
-      </a>
-      <h2 className="text-xl md:text-2xl text-bold mb-2 text-yellow-300">
-        Buffalo Games
-      </h2>
-      <a
-        href="https://buffalogames.com/missing-puzzle-pieces/?gad_source=1&gclid=CjwKCAjwouexBhAuEiwAtW_Zx1ANpnIA9iVzR9R3F4K4U_002syDsYKjwuBWMKClXeSua4l5lK6ZLhoCg0sQAvD_BwE"
-        rel="nooperner noreferrer"
-        className="underline"
-      >
-        Buffalo Games Contact Us
-      </a>
-      <h2 className="text-xl md:text-2xl text-bold mb-2 text-yellow-300">
-        Pomegranate
-      </h2>
-      <a
-        href="https://www.pomegranate.com/pages/contact-us?gad_source=1&gclid=CjwKCAjwouexBhAuEiwAtW_Zxx-93rvro5Ra0rX1EmjqXQcJcoTbFbiKMfypHEG8f9Om9h1rKRfLURoCy1cQAvD_BwE"
-        rel="nooperner noreferrer"
-        className="underline"
-      >
-        Pomegranate Contact Us
-      </a>
+      <div className="border bg-white p-2 rounded w-1/2 mx-auto shadow-lg">
+        <h2 className="text-xl md:text-2xl text-bold mb-2 text-black">
+          Ravensburger
+        </h2>
+        <a
+          href="https://www.ravensburger.us/service/contact-us/index.html"
+          rel="nooperner noreferrer"
+          className="underline text-blue-500"
+        >
+          Ravensburger Contact Us
+        </a>
+      </div>
+      <div className="border bg-white p-2 rounded w-1/2 mx-auto shadow-lg">
+        <h2 className="text-xl md:text-2xl text-bold mb-2 text-black">
+          Cobble Hill
+        </h2>
+        <a
+          href="https://www.cobblehillpuzzles.com/pages/puzzle-issues"
+          rel="nooperner noreferrer"
+          className="underline text-blue-500"
+        >
+          Cobble Hill Contact Us
+        </a>
+      </div>
+      <div className="border bg-white p-2 rounded w-1/2 mx-auto shadow-lg">
+        <h2 className="text-xl md:text-2xl text-bold mb-2 text-black">
+          Buffalo Games
+        </h2>
+        <a
+          href="https://buffalogames.com/missing-puzzle-pieces/?gad_source=1&gclid=CjwKCAjwouexBhAuEiwAtW_Zx1ANpnIA9iVzR9R3F4K4U_002syDsYKjwuBWMKClXeSua4l5lK6ZLhoCg0sQAvD_BwE"
+          rel="nooperner noreferrer"
+          className="underline text-blue-500"
+        >
+          Buffalo Games Contact Us
+        </a>
+      </div>
+      <div className="border bg-white p-2 rounded w-1/2 mx-auto shadow-lg">
+        <h2 className="text-xl md:text-2xl text-bold mb-2 text-black">
+          Pomegranate
+        </h2>
+        <a
+          href="https://www.pomegranate.com/pages/contact-us?gad_source=1&gclid=CjwKCAjwouexBhAuEiwAtW_Zxx-93rvro5Ra0rX1EmjqXQcJcoTbFbiKMfypHEG8f9Om9h1rKRfLURoCy1cQAvD_BwE"
+          rel="nooperner noreferrer"
+          className="underline text-blue-500"
+        >
+          Pomegranate Contact Us
+        </a>
+      </div>
       <div className="flex flex-col items-center text-white text-center text-xl md:text-2xl space-y-6 mt-20 px-6 py-6">
         <h2>I would like to make my own puzzle piece.</h2>
         <button
