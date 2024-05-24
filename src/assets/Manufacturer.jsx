316ff313ce1927737c9cc5d1255b4f8354e5f912
Laura <laura.e.vollmer@gmail.com>
2024-05-23 20:04:59 -0500

    styling manufacturer section

--- src/assets/Manufacturer.jsx
+++ src/assets/Manufacturer.jsx
@@ -2,6 +2,7 @@ import React from "react";
 //importing navigate from react-router-dom
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
+import { FaPuzzlePiece } from "react-icons/fa";
 
 const Manufacturer = () => {
   useEffect(() => {
@@ -20,56 +21,68 @@ const Manufacturer = () => {
         Contact the Manufacturer
       </h1>
       <p>
-        Below are a list of the most popular puzzle manufacturers in the USA. If
-        your manufacturer is not listed, please Google your manufacturer or
-        proceed to making a puzzle piece.
+        Here, we've gathered a list of the top puzzle makers in the USA. If you
+        don't see your preferred manufacturer, feel free to look them up online
+        or go ahead and start creating your puzzle piece.
       </p>
       <div className="border bg-white p-2 rounded w-1/2 mx-auto shadow-lg">
+        <div className="bg-blue-500 rounded-full p-2 inline-flex items-center justify-center">
+          <FaPuzzlePiece className="text-white" />
+        </div>
         <h2 className="text-xl md:text-2xl text-bold mb-2 text-black">
           Ravensburger
         </h2>
         <a
           href="https://www.ravensburger.us/service/contact-us/index.html"
           rel="nooperner noreferrer"
-          className="underline text-blue-500"
+          className="text-blue-500"
         >
-          Ravensburger Contact Us
+          Contact Ravensburger &rarr;
         </a>
       </div>
       <div className="border bg-white p-2 rounded w-1/2 mx-auto shadow-lg">
+        <div className="bg-blue-500 rounded-full p-2 inline-flex items-center justify-center">
+          <FaPuzzlePiece className="text-white" />
+        </div>
         <h2 className="text-xl md:text-2xl text-bold mb-2 text-black">
           Cobble Hill
         </h2>
         <a
           href="https://www.cobblehillpuzzles.com/pages/puzzle-issues"
           rel="nooperner noreferrer"
-          className="underline text-blue-500"
+          className="text-blue-500"
         >
-          Cobble Hill Contact Us
+          Contact Cobble Hill &rarr;
         </a>
       </div>
       <div className="border bg-white p-2 rounded w-1/2 mx-auto shadow-lg">
+        <div className="bg-blue-500 rounded-full p-2 inline-flex items-center justify-center">
+          <FaPuzzlePiece className="text-white" />
+        </div>
         <h2 className="text-xl md:text-2xl text-bold mb-2 text-black">
           Buffalo Games
         </h2>
         <a
           href="https://buffalogames.com/missing-puzzle-pieces/?gad_source=1&gclid=CjwKCAjwouexBhAuEiwAtW_Zx1ANpnIA9iVzR9R3F4K4U_002syDsYKjwuBWMKClXeSua4l5lK6ZLhoCg0sQAvD_BwE"
           rel="nooperner noreferrer"
-          className="underline text-blue-500"
+          className="text-blue-500"
         >
-          Buffalo Games Contact Us
+          Contact Buffalo Games &rarr;
         </a>
       </div>
       <div className="border bg-white p-2 rounded w-1/2 mx-auto shadow-lg">
+        <div className="bg-blue-500 rounded-full p-2 inline-flex items-center justify-center">
+          <FaPuzzlePiece className="text-white" />
+        </div>
         <h2 className="text-xl md:text-2xl text-bold mb-2 text-black">
           Pomegranate
         </h2>
         <a
           href="https://www.pomegranate.com/pages/contact-us?gad_source=1&gclid=CjwKCAjwouexBhAuEiwAtW_Zxx-93rvro5Ra0rX1EmjqXQcJcoTbFbiKMfypHEG8f9Om9h1rKRfLURoCy1cQAvD_BwE"
           rel="nooperner noreferrer"
-          className="underline text-blue-500"
+          className="text-blue-500"
         >
-          Pomegranate Contact Us
+          Contact Pomegranate &rarr;
         </a>
       </div>
       <div className="flex flex-col items-center text-white text-center text-xl md:text-2xl space-y-6 mt-20 px-6 py-6">
