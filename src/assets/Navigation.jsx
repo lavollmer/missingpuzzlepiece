3852fa1918a3e9c8fb6359ac9860bef0f8e7d29f
Laura <laura.e.vollmer@gmail.com>
2024-05-23 20:30:43 -0500

    updating navigation

--- src/assets/Navigation.jsx
+++ src/assets/Navigation.jsx
@@ -20,40 +20,89 @@ function Navigation() {
           isOpen ? "translate-y-0" : "translate-y-full"
         } md:relative md:translate-y-0 md:bg-transparent`}
       >
-        <div className="bg-white h-full md:flex md:items-center md:justify-center md:h-auto md:bg-transparent">
+        <div className="bg-white h-full md:flex md:items-center md:justify-center md:h-auto md:bg-transparent p-5">
           <button
             onClick={() => setIsOpen(false)}
             className="md:hidden block text-black"
           >
             <RiCloseLargeFill size={30} />
           </button>
-          <ul className="md:flex md:space-x-6">
-            <li>
-              <Link to="/" className="block p-4 text-black" onClick={() => setIsOpen(false)}>
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link to="/question" className="block p-4 text-black" onClick={() => setIsOpen(false)}>
-                Questions
-              </Link>
-            </li>
-            <li>
-              <Link to="/manufacturer" className="block p-4 text-black" onClick={() => setIsOpen(false)}>
-                Contact Manufacturer
-              </Link>
-            </li>
-            <li>
-              <Link to="/makingapuzzlepiece" className="block p-4 text-black" onClick={() => setIsOpen(false)}>
-                Make a Puzzle Piece
-              </Link>
-            </li>
-            <li>
-              <Link to="/feedback" className="block p-4 text-black" onClick={() => setIsOpen(false)}>
-                Feedback
-              </Link>
-            </li>
-          </ul>
+          <div className="text-xl">
+            <ul className="md:flex md:space-x-6">
+              <li>
+                <Link
+                  to="/"
+                  className="block p-4 text-black"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/info"
+                  className="block p-4 text-black"
+                  onClick={() => setIsOpen(false)}
+                >
+                  App Information
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/question"
+                  className="block p-4 text-black"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Location Questions
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/manufacturer"
+                  className="block p-4 text-black"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Contact Manufacturer
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/makingapuzzlepiece"
+                  className="block p-4 text-black"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Make a Puzzle Piece
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/feedback"
+                  className="block p-4 text-black"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Feedback
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/result"
+                  className="block p-4 text-black"
+                  onClick={() => setIsOpen(false)}
+                >
+                  Results
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/faq"
+                  className="block p-4 text-black"
+                  onClick={() => setIsOpen(false)}
+                >
+                  FAQ
+                </Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
     </div>
