82714f1e31a049cb54151e6a7172e48ac392c4a0
Laura <laura.e.vollmer@gmail.com>
2024-05-28 14:16:45 -0500

    updating the modal

--- src/assets/Modal.jsx
+++ src/assets/Modal.jsx
@@ -2,28 +2,30 @@ import React from "react";
 import { useState } from "react";
 
 const Modal = () => {
-    const [isOpen,setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-    const toggleModal = () => {
-        setIsOpen(!isOpen);
-    }
+  const toggleModal = () => {
+    setIsOpen(!isOpen);
+  };
 
   return (
     <div>
       <button
-      onClick={toggleModal}
+        onClick={toggleModal}
         data-modal-target="static-modal"
         data-modal-toggle="static-modal"
         class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
       >
-        Toggle modal
+        Notification
       </button>
       <div
         id="static-modal"
         tabIndex="-1"
         aria-hidden="true"
-        className={`overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full ${isOpen ? '' : 'hidden'}`}
+        className={`overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full ${
+          isOpen ? "" : "hidden"
+        }`}
       >
         <div class="relative p-4 w-full max-w-2xl max-h-full">
           <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -57,11 +59,14 @@ const Modal = () => {
 
             <div class="p-4 md:p-5 space-y-4">
               <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-               This application is FREE and currently in the user testing phase. As such, you may encounter some bugs and updates may be required. We appreciate your understanding and patience. If you have any feedback or suggestions, we'd love to hear from you.
+                This application is FREE and currently in the user testing
+                phase. As such, you may encounter some bugs and updates may be
+                required. We appreciate your understanding and patience. If you
+                have any feedback or suggestions, we'd love to hear from you.
               </p>
             </div>
 
-            <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
+            {/* <div class="flex items-center p-4 md:p-5 border-t border-gray-200 rounded-b dark:border-gray-600">
               <button
                 data-modal-hide="static-modal"
                 type="button"
@@ -69,7 +74,7 @@ const Modal = () => {
               >
                 Thanks for the reminder!
               </button>
-            </div>
+            </div> */}
           </div>
         </div>
       </div>
