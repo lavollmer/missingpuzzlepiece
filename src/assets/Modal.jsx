3b6caca07d88f0270109fe0f5badeae0fec2ca0b
Laura <laura.e.vollmer@gmail.com>
2024-05-28 14:31:50 -0500

    debugging modal

--- src/assets/Modal.jsx
+++ src/assets/Modal.jsx
@@ -2,11 +2,19 @@ import React from "react";
 import { useState } from "react";
 
 const Modal = () => {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpen, setIsOpen] = useState(true);
 
   const toggleModal = () => {
     setIsOpen(!isOpen);
   };
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
+  if (!isOpen) {
+    return null;
+  }
 
   return (
     <div>
@@ -14,11 +22,12 @@ const Modal = () => {
         onClick={toggleModal}
         data-modal-target="static-modal"
         data-modal-toggle="static-modal"
-        class="block text-white bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 font-montserrat font-medium"
+        className="block text-white text-lg bg-green-500 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 rounded-lg  px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 font-montserrat font-medium"
         type="button"
       >
         Developer Notification
       </button>
+      {isOpen && <Modal handleClose={toggleModal} />}
       <div
         id="static-modal"
         tabIndex="-1"
@@ -27,14 +36,14 @@ const Modal = () => {
           isOpen ? "" : "hidden"
         }`}
       >
-        <div class="relative p-4 w-full max-w-2xl max-h-full font-montserrat font-medium">
+        <div class="relative p-4 w-full max-w-xl max-h-full font-montserrat font-medium">
           <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-              <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
+              <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
                 Notification
               </h3>
               <button
-              onClick={toggleModal}
+                onClick={handleClose}
                 type="button"
                 class="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
                 data-modal-hide="static-modal"
@@ -60,13 +69,13 @@ const Modal = () => {
 
             <div class="p-4 md:p-5 space-y-4">
               <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                This application is free and currently in the user testing
-                phase. As such, you may encounter some bugs and updates may be
-                required. I appreciate your understanding and patience. If you
-                have any feedback or suggestions, we'd love to hear from you. Thank you for supporting Find My Puzzle Piece!
+                This application is currently in the user testing phase. As
+                such, you may encounter some bugs and updates may be required. I
+                appreciate your understanding and patience. If you have any
+                feedback or suggestions, I'd love to hear from you. Thank you
+                for supporting Find My Puzzle Piece!
               </p>
             </div>
-
           </div>
         </div>
       </div>
