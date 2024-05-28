48b6a18f80e4cf0f9fd47659da44c9458d02d2e7
Laura <laura.e.vollmer@gmail.com>
2024-05-28 14:12:12 -0500

    updating modal button

--- src/assets/Modal.jsx
+++ src/assets/Modal.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useState } from "react";
 
 const Modal = () => {
+    const [isOpen,setIsOpen] = useState(false);
+
+    const toggleModal = () => {
+        setIsOpen(!isOpen);
+    }
+
   return (
     <div>
       <button
+      onClick={toggleModal}
         data-modal-target="static-modal"
         data-modal-toggle="static-modal"
         class="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
@@ -13,16 +21,15 @@ const Modal = () => {
       </button>
       <div
         id="static-modal"
-        data-modal-backdrop="static"
-        tabindex="-1"
+        tabIndex="-1"
         aria-hidden="true"
-        class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
+        className={`overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full ${isOpen ? '' : 'hidden'}`}
       >
         <div class="relative p-4 w-full max-w-2xl max-h-full">
           <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
               <h3 class="text-xl font-semibold text-gray-900 dark:text-white">
-                Static modal
+                Notification
               </h3>
               <button
                 type="button"
@@ -50,16 +57,7 @@ const Modal = () => {
 
             <div class="p-4 md:p-5 space-y-4">
               <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                With less than a month to go before the European Union enacts
-                new consumer privacy laws for its citizens, companies around the
-                world are updating their terms of service agreements to comply.
-              </p>
-              <p class="text-base leading-relaxed text-gray-500 dark:text-gray-400">
-                The European Union’s General Data Protection Regulation
-                (G.D.P.R.) goes into effect on May 25 and is meant to ensure a
-                common set of data rights in the European Union. It requires
-                organizations to notify users as soon as possible of high-risk
-                data breaches that could personally affect them.
+               This application is FREE and currently in the user testing phase. As such, you may encounter some bugs and updates may be required. We appreciate your understanding and patience. If you have any feedback or suggestions, we'd love to hear from you.
               </p>
             </div>
 
@@ -67,16 +65,9 @@ const Modal = () => {
               <button
                 data-modal-hide="static-modal"
                 type="button"
-                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                class="text-white text-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
               >
-                I accept
-              </button>
-              <button
-                data-modal-hide="static-modal"
-                type="button"
-                class="py-2.5 px-5 ms-3 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700"
-              >
-                Decline
+                Thanks for the reminder!
               </button>
             </div>
           </div>
